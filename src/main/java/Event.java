bc6ab114985863fe7ecfe56cf1ceee0aac5458b8
Steve Zaske <github@zaske.com>
2017-06-30 08:49:43 -0700

    include Event class constructor

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,27 @@
 public class Event {
-  private int mGuests; //number of guests
+  private String mName; //name of event
+  private int mGuestCount; //number of guests
   private String mFood; //what food is to be served
+  private String mBeverages; //Beverage choice for event
+  private String mEntertainment; //what entertainment was chosen for the event
 
-  public void setGuests(int guests) {
-    mGuests = guests;
+  public void setGuestCount(int guests) {
+    mGuestCount = guests;
   }
 
-  public int getGuests(){
-    return mGuests;
+  public int getGuestCount(){
+    return mGuestCount;
+  }
+
+  public void setFood(String foodChoice) {
+    mFood = foodChoice;
+  }
+
+  public String getFood(){
+    return mFood;
+  }
+
+  public Event(String name){ //Class constructor
+    mName = name;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,15 +5,15 @@ public class EventTest {
 
   @Test
     public void newEvent_instantiatesCorrectly() {
-      Event testEvent = new Event();
+      Event testEvent = new Event("test");
       assertEquals(true, testEvent instanceof Event);
     }
 
   @Test
-    public void setGuests_isAbleToSetGuestsCount_100() {
-      Event testEvent = new Event();
-      testEvent.setGuests(100);
-      assertEquals(100, testEvent.getGuests());
+    public void setGuestCount_isAbleToSetGuestsCount_100() {
+      Event testEvent = new Event("test");
+      testEvent.setGuestCount(100);
+      assertEquals(100, testEvent.getGuestCount());
     }
 
 }
